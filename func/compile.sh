a5846a461a9117a7c1ede3f282b91d01875ec1da
ice-charon <antoniy.shumanov@leftclick.io>
2024-08-27 15:43:57 +0300

    Replace ton to ion
    Signed-off-by: ice-charon <antoniy.shumanov@leftclick.io>

--- func/compile.sh
+++ func/compile.sh
@@ -1,5 +1,6 @@
 rm build/nft-item-code.fif
 rm build/nft-collection-code.fif
+rm build/root-dns-code.fif
 
 func -o build/nft-item-code.fif -SPA stdlib.fc params.fc op-codes.fc dns-utils.fc nft-item.fc
 func -o build/nft-collection-code.fif -SPA stdlib.fc params.fc op-codes.fc dns-utils.fc nft-collection.fc
